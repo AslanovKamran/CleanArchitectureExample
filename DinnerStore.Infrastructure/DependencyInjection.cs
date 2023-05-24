79f80117dbded91a1dc9db5c2b0431a334539ad6
Aslan17 <k.aslan1707@gmail.com>
2023-05-24 16:52:09 +0400

    Jwt Tokens Added (end of part 2)

--- DinnerStore.Infrastructure/DependencyInjection.cs
+++ DinnerStore.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DinnerStore.Application.Common.Interfaces.Authentication;
+using DinnerStore.Infrastructure.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using DinnerStore.Application.Common.Interfaces.Services;
+using DinnerStore.Infrastructure.Services;
 
 namespace DinnerStore.Infrastructure
 {
 	public static class DependencyInjection
 	{
-		public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
 		{
+			services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+			services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+			services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 			return services;
 		}
 	}
---
+++ DinnerStore.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿
+using DinnerStore.Application.Common.Interfaces.Services;
+
+namespace DinnerStore.Infrastructure.Services
+{
+	public class DateTimeProvider : IDateTimeProvider
+	{
+		public DateTime UtcNow => DateTime.UtcNow;
+	}
+}
