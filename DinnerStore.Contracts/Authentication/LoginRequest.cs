e5fe26ba678e1a55619f4aa8b55569f56c855a16
Aslan17 <k.aslan1707@gmail.com>
2023-05-31 16:35:32 +0400

    Validation Added (end of part 8)

--- DinnerStore.Contracts/Authentication/LoginRequest.cs
+++ DinnerStore.Contracts/Authentication/LoginRequest.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DinnerStore.Contracts.Authentication
 {
 	public record LoginRequest(
+		
+		[Required(AllowEmptyStrings =false)]		
 		string Email,
+		
+		[Required(AllowEmptyStrings =false)]
 		string Password
 		);
 }
--- DinnerStore.Contracts/Authentication/RegisterRequest.cs
+++ DinnerStore.Contracts/Authentication/RegisterRequest.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DinnerStore.Contracts.Authentication
 {
 	public record RegisterRequest(
+		[Required(AllowEmptyStrings =false)]
 		string FirstName,
+		[Required(AllowEmptyStrings =false)]
 		string LastName,
+		[Required(AllowEmptyStrings =false)]
 		string Email,
+		[Required(AllowEmptyStrings =false)]
 		string Password
 		);
 }
